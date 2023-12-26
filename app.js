a8c76eef2a4b7dee5c9066d087f1ec6bc1616f84
Emerson602 <wemerson.azevedo@iqb.ufal.br>
2023-12-26 15:29:16 -0300

    update

---
+++ app.js
@@ -0,0 +1,17 @@
+const app = {
+    data() {
+        return {
+            taskInput: '',
+                 
+        }
+    },
+    methods: {
+
+        
+    },
+    mounted() {
+       
+    }
+}
+
+Vue.createApp(app).mount('#app');
