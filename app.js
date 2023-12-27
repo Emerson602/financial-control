b5e4d94c891c078c218ff29303a8e75deddd7495
Emerson602 <wemerson.azevedo@iqb.ufal.br>
2023-12-27 17:15:05 -0300

    update

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const app = {
     data() {
         return {
             showForm: false,
+            showHistoric: false,
                  
         }
     },
     methods: {
         toggleForm() {
             this.showForm = !this.showForm;
-          }        
+          },
+        toggleHistoric() {
+            this.showHistoric = !this.showHistoric;
+          },          
     },
     mounted() {
        
