fdc81e68b7825b15266676883b2a9e4cc3435fdb
Emerson602 <wemerson.azevedo@iqb.ufal.br>
2024-01-04 17:52:57 -0300

    update

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ const app = {
             setInterval(() => {                  
                   const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'};                  
                   const formattedDateTime = this.currentDateTime.toLocaleDateString('pt-BR', options);                                         
-                  localStorage.setItem("DateTime", formattedDateTime);
-                  this.dataTime = localStorage.getItem("DateTime")                                 
+                  localStorage.setItem("dateTime", formattedDateTime);
+                  this.dateTime = localStorage.getItem("dateTime")                                 
             }, 10000) 
          }, 
         setCurrentYear() {            
@@ -47,38 +47,41 @@ const app = {
         },
         
         getTransactionHistory() {
-        /*  this.transactionList = [];
+           this.transactionList = [];
             let count = 0;
             for (let i = 0; i < this.numberTransaction; i++) {
                 count += 1;
-
-                const transactionDate = localStorage.getItem(`transaction-date-${this.numberTransaction}`, this.dataTime);
-                const transactionDescription = localStorage.getItem(`transaction-description-${this.numberTransaction}`, this.descriptionInput);
-                const transactionType = localStorage.getItem(`transaction-type-${this.numberTransaction}`, this.typeInput);
-                const transactionValue = localStorage.getItem(`transaction-value-${this.numberTransaction}`, this.valueInput);
-                const numberTransaction = localStorage.getItem("number-transaction", this.numberTransaction);
                 
-                this.transactionList.push(`<span id="date-time" class="bg-light">${createIn}</span><span class="bg-light  mt-3">${transactionDescription}</span>`); 
-                `<li class="d-flex flex-row justify-content-between align-items-center bg-light p-3 border border-success border-5 border-start-0 border-start-top border-bottom-0 border-top-0 rounded mt-2">
-                    <span>Salario</span>                    
-                    <span>R$ 4000</span>
-                 </li>`
-            }    */
+                const transactionDate = localStorage.getItem(`transaction-date-${count}`, this.dateTime);
+                const transactionDescription = localStorage.getItem(`transaction-description-${count}`, this.descriptionInput);
+                const transactionType = localStorage.getItem(`transaction-type-${count}`, this.typeInput);
+                const transactionValue = localStorage.getItem(`transaction-value-${count}`, this.valueInput);
+                                
+                this.transactionList.push(`      
+                    <span id="data" class="text-center">${transactionDate}</span>                         
+                    <span class="mt-2 text-start text-sm-center d-block">Descrição: <br>${transactionDescription}</span>
+                    <span class="mt-2 text-start text-sm-center d-block">Tipo: ${transactionType}</span>                
+                    <span class="text-start text-sm-center d-block">Valor: ${transactionValue}</span>                                           
+                `); 
+
+            }    
         
         },  
         newTransaction() {         
 
-            if(this.descriptionInput !== '' && this.typeInput === 'Receita' && this.valueInput !== '' && !isNaN(this.valueInput)) {               
-                this.financialIncome += parseInt(this.valueInput)   
+            if(this.descriptionInput !== '' && this.typeInput === 'Receita' && this.valueInput !== '' && !isNaN(this.valueInput)) {   
+                this.financialIncome += parseInt(this.valueInput)
+                
                 this.storeData()
                 this.toggleForm() 
                 this.reload() 
-                this.currentBalance()                  
+                this.currentBalance()                                  
                 return        
             }
-
-            if(this.descriptionInput !== '' && this.typeInput === 'Despesa' && this.valueInput !== '' && !isNaN(this.valueInput)) {                
+            
+            if(this.descriptionInput !== '' && this.typeInput === 'Despesa' && this.valueInput !== '' && !isNaN(this.valueInput)) {  
                 this.financialExpenses += parseInt(this.valueInput)
+
                 this.storeData()
                 this.toggleForm()
                 this.reload()  
@@ -92,7 +95,7 @@ const app = {
         storeData() {
                 this.numberTransaction += 1;
                     
-                localStorage.setItem(`transaction-date-${this.numberTransaction}`, this.dataTime); 
+                localStorage.setItem(`transaction-date-${this.numberTransaction}`, this.dateTime); 
                 localStorage.setItem(`transaction-description-${this.numberTransaction}`, this.descriptionInput);
                 localStorage.setItem(`transaction-type-${this.numberTransaction}`, this.typeInput);
                 localStorage.setItem(`transaction-value-${this.numberTransaction}`, this.valueInput);
@@ -136,7 +139,7 @@ const app = {
         reload() {
             setTimeout(() => {
                 location.reload()
-            }, 500); 
+            }, 200); 
         },              
     },
     mounted() {
