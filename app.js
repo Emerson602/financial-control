f85566e5c6edfbc5c19996655ff6a0d170f89dab
Emerson602 <wemerson.azevedo@iqb.ufal.br>
2024-01-11 18:06:31 -0300

    update

--- app.js
+++ app.js
@@ -15,51 +15,119 @@ const app = {
             numberTransaction: parseInt(localStorage.getItem("number-transaction") || 0),  
             applyColor: 'success',
             historyType: '', 
-            headerVisible: false,                                          
+            headerVisible: false,   
+            showModal: false,
+            currentDay: '',
+            currentMonth: '',
+            currentYear: '',
+            currentHour: '',
+            currentMinute: '',            
         }
     },
     methods: {
 
         getCurrentDateTime() {
+
             setInterval(() => {                  
                   const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'};                  
                   const formattedDateTime = this.currentDateTime.toLocaleDateString('pt-BR', options);                                         
                   localStorage.setItem("dateTime", formattedDateTime);
-                  this.dateTime = localStorage.getItem("dateTime")                                 
-            }, 1000) 
+                  this.dateTime = localStorage.getItem("dateTime") 
+
+                  this.convertCurrentDate()                                                     
+            }, 2000) 
          }, 
-        setCurrentYear() {            
+
+         convertCurrentDate() {
+
+            let dateTimeString = this.dateTime.split(' ')                     
+            let dayNumber = parseInt(dateTimeString [0]) 
+            let monthNumber =  dateTimeString [2]
+            let yearNumber =  parseInt(dateTimeString [4])
+            let timeString =  dateTimeString [6]
+
+            timeString = timeString.split(':')
+
+            let minuteNumber = parseInt(timeString[0])
+            let hoursNumber = parseInt(timeString[1]) 
+            
+            if(monthNumber === 'janeiro') {                
+                this.currentMonth = 1
+            } else if(monthNumber === 'fevereiro') {
+                this.currentMonth = 2
+            } else if(monthNumber === 'março') {
+                this.currentMonth = 3
+            } else if(monthNumber === 'abril') {
+                this.currentMonth = 4
+            } else if(monthNumber === 'maio') {
+                this.currentMonth = 5
+            } else if(monthNumber === 'junho') {
+                this.currentMonth = 6
+            } else if(monthNumber === 'julho') {
+                this.currentMonth = 7
+            } else if(monthNumber === 'agosto') {
+                this.currentMonth = 8
+            } else if(monthNumber === 'setembro') {
+                this.currentMonth = 9
+            } else if(monthNumber === 'outubro') {
+                this.currentMonth = 10
+            } else if(monthNumber === 'novembro') {
+                this.currentMonth = 11
+            } else if(monthNumber === 'dezembro') {
+                this.currentMonth = 12
+            } 
+
+            this.currentDay = dayNumber    
+            this.currentYear = yearNumber
+            this.currentHour = minuteNumber
+            this.currentMinute = hoursNumber          
+         },
+
+        setCurrentYear() {         
+
             const footer = document.querySelector('#footer');   
             
             footer.innerText = `Financial control © - ${this.currentDateTime.getFullYear()}`
         },
+
         scrollToHome() {
+
             const home = document.querySelector('#app')
-            home.scrollIntoView({ behavior: 'smooth' });       
+            home.scrollIntoView({ behavior: 'smooth' });   
         },
+
         scrollToForm() {
+
             const form = document.querySelector('#transaction-form')
             form.scrollIntoView({ behavior: 'smooth' });    
 
             this.homeButton = false
-            this.showHomeButton()                         
+            this.showHomeButton()   
         },
+
         scrollToHistoric() {
+
             const historic = document.querySelector('#historic')
             historic.scrollIntoView({ behavior: 'smooth' });
 
             this.homeButton = false
-            this.showHomeButton()       
+            this.showHomeButton()   
         },
+
         checkTransaction() {
+
             if(this.numberTransaction <= 0) {
                 this.noTransaction = !this.noTransaction; 
             } 
-        },    
+        }, 
+
         showHomeButton() {
+
             this.homeButton = !this.homeButton;
         },
+
         handleHeaderIntersection(entries) {
+
             entries.forEach(entry => {
               if (entry.isIntersecting) {               
                 this.headerVisible = true;
@@ -67,31 +135,102 @@ const app = {
               } else {                
                 this.headerVisible = false;                
               }
-            });
+            });            
+        },
 
-          },
           handleHeaderVisibility() {
+
             this.homeButton = true
             this.showHomeButton()
-          },           
+
+        },     
+
         getTransactionHistory() {
+
            this.transactionList = [];           
 
             let count = 0;
 
             for (let i = 0; i < this.numberTransaction; i++) {
+
                 count += 1;
                  
                 const transactionDate = localStorage.getItem(`transaction-date-${count}`, this.dateTime);
                 const transactionDescription = localStorage.getItem(`transaction-description-${count}`, this.descriptionInput);
                 const transactionType = localStorage.getItem(`transaction-type-${count}`, this.typeInput);
-                const transactionValue = localStorage.getItem(`transaction-value-${count}`, this.valueInput);              
+                const transactionValue = localStorage.getItem(`transaction-value-${count}`, this.valueInput);       
                 
+                // inicio 
+                    
+                let newTransactionString = transactionDate.split(" ");
+                let index = count;
+                let day = parseInt(newTransactionString[0]);
+                let monthString = newTransactionString[2];
+                let month; 
+                
+                if(monthString === 'janeiro') {
+                    month = 1
+                } else if(monthString === 'fevereiro') {
+                    month = 2
+                } else if(monthString === 'março') {
+                    month = 3
+                } else if(monthString === 'abril') {
+                    month = 4
+                } else if(monthString === 'maio') {
+                    month = 5
+                } else if(monthString === 'junho') {
+                    month = 6
+                } else if(monthString === 'julho') {
+                    month = 7
+                } else if(monthString === 'agosto') {
+                    month = 8
+                } else if(monthString === 'setembro') {
+                    month = 9
+                } else if(monthString === 'outubro') {
+                    month = 10
+                } else if(monthString === 'novembro') {
+                    month = 11
+                } else if(monthString === 'dezembro') {
+                    month = 12
+                }  
+                
+                let year = parseInt(newTransactionString[4])
+                let time = newTransactionString[6] 
+
+                let newTimeString = time.split(":")                 
+
+                let hours = parseInt(newTimeString[0])
+                let minutes = parseInt(newTimeString[1])
+
+                console.log(`Indice ${index}: ${day} ${month} ${year} ${hours}:${minutes}`) 
+                console.log(`Indice ${index}: ${day} ${month} ${year + 1} ${hours}:${minutes}`) 
+                
+
+                if(this.currentDay >= day && this.currentMonth >= month + 6 && this.currentYear >= year) {                     
+                    localStorage.removeItem(`transaction-date-${count}`, this.dateTime);
+                    localStorage.removeItem(`transaction-description-${count}`, this.descriptionInput);
+                    localStorage.removeItem(`transaction-type-${count}`, this.typeInput);
+                    localStorage.removeItem(`transaction-value-${count}`, this.valueInput); 
+
+                    this.transactionList.pop()
+                    this.reload()
+                } else if(this.currentDay >= 1 && this.currentMonth > month + 6 && this.currentYear >= year) {                     
+                    localStorage.removeItem(`transaction-date-${count}`, this.dateTime);
+                    localStorage.removeItem(`transaction-description-${count}`, this.descriptionInput);
+                    localStorage.removeItem(`transaction-type-${count}`, this.typeInput);
+                    localStorage.removeItem(`transaction-value-${count}`, this.valueInput); 
+
+                    this.transactionList.pop()
+                    this.reload()
+                }          
+                
+                // final
+
                 this.historyType = transactionType;
 
                 this.setColor();
    
-                this.transactionList.push(`  
+                this.transactionList.unshift(`  
                   <div class="d-flex flex-column align-items-center justify-content-center bg-light p-3 border-5 border-start-0 border-start-top border-bottom-0 border-top-0 rounded mt-2  border border-${this.applyColor}">    
                     
                     <span id="data" class="text-center">${transactionDate}</span>   
@@ -106,10 +245,17 @@ const app = {
                     
                   </div>                                             
                 `);                
+                
             }  
-
         }, 
-        setColor() {            
+
+        deleteOldTransactions() {
+
+           
+        },
+        
+        setColor() {   
+
             if(this.historyType === 'Despesa') {
                 this.applyColor = 'danger'               
                 return
@@ -119,6 +265,7 @@ const app = {
                 this.applyColor = 'success'
             }
         },
+
         newTransaction() {         
 
             if(this.descriptionInput !== '' && this.typeInput === 'Receita' && this.valueInput !== '' && this.valueInput > 0 && !isNaN(this.valueInput)) {   
@@ -132,14 +279,27 @@ const app = {
                 return
             }
 
-            this.storeData() 
-            this.scrollToHome() 
-            this.checkTransaction()              
-            this.reload()  
-            this.currentBalance()              
- 
-        },         
+            this.storeData()              
+            this.checkTransaction()        
+            this.currentBalance()
+            this.scrollToHome()
+            this.openModal()                   
+        }, 
+        
+        openModal() {
+        
+            this.showModal = true; 
+        },
+
+        closeModal() { 
+
+            this.showModal = false; 
+            this.scrollToHome()                  
+            this.reload()
+        },
+
         storeData() {
+
                 this.numberTransaction += 1;
                     
                 localStorage.setItem(`transaction-date-${this.numberTransaction}`, this.dateTime); 
@@ -150,17 +310,20 @@ const app = {
 
                 this.getTransactionHistory()
         },
-        currentBalance() {                         
+
+        currentBalance() {  
+
             localStorage.setItem("financialIncome", this.financialIncome);
             localStorage.setItem("financialExpenses", this.financialExpenses);
 
             this.financialIncome = parseInt(localStorage.getItem("financialIncome"));
             this.financialExpenses = parseInt(localStorage.getItem("financialExpenses"));  
         
-            this.amount = this.financialIncome - this.financialExpenses;          
-
+            this.amount = this.financialIncome - this.financialExpenses;        
         },
+
         showCurrentBalance() {
+
             const currentBalance = document.querySelector('#current-balance');
             const financialIncome = document.querySelector('#financial-income');
             const financialExpenses = document.querySelector('#financial-expenses');       
@@ -183,15 +346,18 @@ const app = {
               currentBalance.textContent = formatNumber(amountString);
               financialIncome.textContent = formatNumber(financialIncomeString);
               financialExpenses.textContent = formatNumber(financialExpensesString);  
-
         },
+
         reload() {
+
             setTimeout(() => {
                 location.reload()
             }, 1000); 
         },              
     },
+    
     mounted() {       
+
        this.getCurrentDateTime()
        this.checkTransaction()
        this.setCurrentYear()
@@ -208,7 +374,8 @@ const app = {
       });
 
         observer.observe(headerElement);
-    }
+
+    }  
 }
 
 Vue.createApp(app).mount('#app');
